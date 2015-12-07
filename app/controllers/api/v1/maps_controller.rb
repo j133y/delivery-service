fb5adcdbd3337cb0b16667715bfd28087e5e3830
Jimmy <alexandre.sns@gmail.com>
2015-12-06 23:34:34 -0200

    Add documentation to map resource endpoints

--- app/controllers/api/v1/maps_controller.rb
+++ app/controllers/api/v1/maps_controller.rb
@@ -1,13 +1,62 @@
 class Api::V1::MapsController < ApplicationController
   before_action :set_map, only: [:show, :edit, :update, :destroy]
 
+  # Public: Action to list all the maps registered.
+  #
+  # Examples:
+  #   GET /maps
+  #
+  # Returns a json with a list of all maps with routes.
   def index
     @maps = Map.all
   end
 
+  # Public: Action to retrieve a map for a given id.
+  #
+  # id - Integer value corresponding to map id.
+  #
+  # Examples:
+  #   GET /maps/1
+  #
+  # Returns a json with a list of all maps with routes.
   def show
   end
 
+  # Public: Action to create a map with given parameters.
+  #
+  # map - Hash with map parameters.
+  #        :name - String for the map name.
+  #        :route_attributes - An Array of route objects.
+  #              :origin - String with the origin route name.
+  #              :destination - String with the destination route name.
+  #              :distance - Integer with the distante between origin and destination.
+  #
+  # Examples:
+  #   POST /maps
+  #   Body:
+  #   {
+  #     "name": "Mapa SP",
+  #     "routes": [
+  #       {
+  #         "origin": "Barueri",
+  #         "destination": "Sao Paulo",
+  #         "distance": 50
+  #       },
+  #       {
+  #         "origin": "Osasco",
+  #         "destination": "Sao Paulo",
+  #         "distance": 30
+  #       },
+  #       {
+  #         "origin": "Sao Paulo",
+  #         "destination": "Santos",
+  #         "distance": 100
+  #       }
+  #     ]
+  #   }
+  #
+  # Returns a json with the representation of the given map if created.
+  # Returns a exception json with status 422 if the map was not created.
   def create
     @map = Map.new(map_params)
 
@@ -20,6 +69,48 @@ class Api::V1::MapsController < ApplicationController
     end
   end
 
+  # Public: Action to update a map with given parameters.
+  #
+  # map - Hash with map parameters.
+  #        :id - Integer with the map id to be updated.
+  #        :name - String for the map name.
+  #        :route_attributes - An Array of route objects.
+  #              :id - Integr with the route id to be updated.
+  #              :origin - String with the origin route name.
+  #              :destination - String with the destination route name.
+  #              :distance - Integer with the distante between origin and destination.
+  #
+  # Examples:
+  #   POST /maps
+  #   Body:
+  #   {
+  #     "id": 1,
+  #     "name": "Mapa SP",
+  #     "routes": [
+  #       {
+  #         "id": 1,
+  #         "origin": "Barueri",
+  #         "destination": "Sao Paulo",
+  #         "distance": 50
+  #       },
+  #       {
+  #         "id": 2,
+  #         "origin": "Osasco",
+  #         "destination": "Sao Paulo",
+  #         "distance": 30
+  #       },
+  #       {
+  #         "id": 3,
+  #         "origin": "Sao Paulo",
+  #         "destination": "Santos",
+  #         "distance": 100
+  #       }
+  #     ]
+  #   }
+  #
+  # Returns a json with the representation of the given map if updated.
+  # Returns a exception json with status 404 if the map was not found.
+  # Returns a exception json with status 422 if the map was not updated.
   def update
     respond_to do |format|
       if @map.update(update_map_params)
@@ -30,6 +121,15 @@ class Api::V1::MapsController < ApplicationController
     end
   end
 
+  # Public: Action to delete an existing map.
+  #
+  # id - Integer with the map id to be deleted.
+  #
+  # Examples:
+  #   DELETE /maps/:id
+  #
+  # Returns nothing if success.
+  # Returns a exception json with status 404 if the map was not found.
   def destroy
     @map.destroy
 
@@ -38,6 +138,28 @@ class Api::V1::MapsController < ApplicationController
     end
   end
 
+  # Public: Action to find the most economical path for a given delivery.
+  #
+  # map - Hash with map parameters.
+  #        :name - String for the map name.
+  #        :origin - String for the delivery origin.
+  #        :destination - String for the delivery destination.
+  #        :liter_price - String for the fuel price per liter.
+  #        :autonomy - String for the autonomy of the transportation.
+  #
+  # Examples:
+  #   POST /maps/estimate_delivery
+  #   Body:
+  #   {
+  #     "name": "Mapa SP",
+  #     "origin": "Barueri",
+  #     "destination": "Sao Paulo",
+  #     "liter_price": 2.50,
+  #     "autonomy": 10
+  #   }
+  #
+  # Returns a json with the representation of the delivery route path with cost.
+  # Returns a exception json with status 404 if the map was not found.
   def estimate_delivery
     @map = Map.where('lower(name) = ?', estimate_delivery_params[:name].try(:downcase)).first
 
